3c8aa965763fe7ba3d2ad980fae0f23f23425102
JuanZapataO6 <juan.zapataoc@gmail.com>
2022-04-13 12:06:47 -0500

    Primer Commit Curso Spring Platzi

---
+++ src/main/java/com/platzi/martket/web/controller/PlatziMartketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.martket.web.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMartketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMartketApplication.class, args);
+	}
+
+}
